c28c008b83211d5b8d8b3a6bd574961afc579a61
andecodelima <andrelimag89@gmail.com>
2025-09-04 14:48:56 -0300

    theme add

--- react/chronos-pomodoro/src/App.tsx
+++ react/chronos-pomodoro/src/App.tsx
@@ -10,32 +10,12 @@ import { Menu } from "./components/Menu";
 import "./styles/global.css";
 import "./styles/theme.css";
 import { Footer } from "./components/Footer";
-import { Heading } from "./components/Heading";
-import { useState } from "react";
 
 export function AppTeste() {
 
-  // Quero que todos os componentes que usam "numero" saibam das mudanças no
-  // seu valor.
-
-  // let numero = 0;
-
-  // Sempre que usar useState nao usar atribuição diretamente.
-  const [numero, setNumero ] = useState(0);
-
-  function handleClick(){
-    setNumero(numero + 1);
-
-    // Guardando o estado anterior do numero
-    // setNumero(prevState => prevState + 1);
-  }
 
   return (
     <>
-      
-      <Heading>{numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
-
       <Container>
         <Logo/>
       </Container>
@@ -55,7 +35,7 @@ export function AppTeste() {
            <DefaultInput 
               type="text"
               id="meuInput" 
-              labelText={numero.toString()}
+              labelText="Any"
               placeholder="Digite algo"
               // disabled
               // defaultValue="Valor preenchido"
--- react/chronos-pomodoro/src/components/Menu/index.tsx
+++ react/chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,19 +1,73 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState, useEffect } from "react";
 
+type AvalilableThemes = 'dark' | 'light'
 export function Menu() {
+
+  const [theme, setTheme] = useState<AvalilableThemes>('dark');
+  
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ){
+    event.preventDefault();
+
+    setTheme(preventTheme =>{
+      const nextThme = preventTheme === 'dark'? 'light' : 'dark'
+      return nextThme;
+    });
+   }
+
+  //  useEffect(()=>{
+  //   console.log('useEffect com array deps vazio', Date.now());
+  //  });//Executado toda vez que o componenente renderiza na tela.
+
+
+  //   useEffect(()=>{
+  //   console.log('useEffect com array deps vazio', Date.now());
+  //  },[]);//Executado apenas uma vez quando o Rezct monta o componente na tela pela primeira vez
+
+
+    useEffect(()=>{
+      document.documentElement.setAttribute('data-theme', theme);
+
+      return() =>{
+        console.log('limpeza de trash atualizado')
+      }
+   }, [theme]);// Executado apenas quando o valor da função for alterado. 
+
+
+
   return (
     <nav className={styles.menu}>
-      <a href="#" className={styles.menuLink}>
+      {/* <h1>{theme}</h1> */}
+      <a href="#" 
+      className={styles.menuLink}
+      aria-label="Ir para a Home"
+      title="Ir para a Home">
         <HouseIcon/>
       </a>
-      <a href="#" className={styles.menuLink}>
+
+      <a href="#" 
+      className={styles.menuLink}
+      aria-label="Ver histórico"
+      title="Ver histórico">
         <HistoryIcon/>
       </a>
-      <a href="#" className={styles.menuLink}>
+
+      <a href="#" 
+      className={styles.menuLink}
+      aria-label="Configurações"
+      title="Configurações">
         <SettingsIcon/>
       </a>
-      <a href="#" className={styles.menuLink}>
+
+      <a href="#" 
+      className={styles.menuLink}
+      aria-label="Alterar o Tema da Página"
+      title="Alterar o Tema da Página"
+      onClick={handleThemeChange}
+      >
         <SunIcon/>
       </a>
     </nav>
